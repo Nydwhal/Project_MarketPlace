4eb2e204410acc3cd98611fe37644326f99ca11b
Nydwhal <cyril.cauquil@gmail.com>
2022-02-28 17:18:48 +0100

    Add config for authentication

--- Backend-MarketPlace/routes/api.php
+++ Backend-MarketPlace/routes/api.php
@@ -3,5 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('user', [UserController::class, 'index']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::get('/user-profile', [AuthController::class, 'userProfile']); 
